ccdd6249b59981a5b5e625751960a195d50931a4
alihsz <57255882+alihsz@users.noreply.github.com>
2023-02-05 23:17:14 +0330

    feat : nft contract

--- contracts/nft.sol
+++ contracts/nft.sol
@@ -10,18 +10,22 @@ import "./nft_explorer.sol";
 
 contract nft is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
-
+    Counters.Counter private _iterationCounter;
     Counters.Counter private _tokenIdCounter;
+    string private publicURI;
+    address private _owner;
     address public _nftExplorerAddress; //NFT EXPLORER ADDRESS(!!2) --- !!0
 
-    constructor() ERC721("MyToken", "MTK") {}
+    mapping(uint256 => mapping(address => uint256)) private req_track;
+
+    constructor() ERC721("XpNFT", "XNFT") {}
 
     function safeMint(address to, string memory uri) public onlyOwner {
         _tokenIdCounter.increment();
         uint256 tokenId = _tokenIdCounter.current();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
-        nft_explorer(_nftExplorerAddress).mapNFT(to, 3111); ///XP VALUE IS JUST HARDCODED --- !!1
+        nft_explorer(_nftExplorerAddress).mapNFT(to, 0); ///XP VALUE IS JUST HARDCODED --- !!1
     }
 
     // The following functions are overrides required by Solidity.
@@ -39,7 +43,45 @@ contract nft is ERC721, ERC721URIStorage, Ownable {
         override(ERC721, ERC721URIStorage)
         returns (string memory)
     {
+        return publicURI;
+    }
+
+    //////////FOR OPENSEA   -- IT SHOULD CHANGE
+    function contractURI() public pure returns (string memory) {
+        return "ipfs://QmTugTgbD2nuc86JuPpmL6sn9MEM2uXwpumHL1AMaGeJnk";
+    }
+
+    function set_public_URI(string memory URI) public onlyOwner {
+        publicURI = URI;
+    }
+
+    function verify_URI_access(uint256 tokenId) public {
+        require(
+            ownerOf(tokenId) == msg.sender,
+            "Ownable: caller is not the owner"
+        );
+        req_track[_iterationCounter.current()][msg.sender] = block.timestamp;
+    }
+
+    function get_private_URI(address owner, uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        require(
+            (req_track[_iterationCounter.current()][owner]) > 0,
+            "block time has been changed, please verify access again."
+        );
+        require(ownerOf(tokenId) == owner, "Ownable: caller is not the owner");
         return super.tokenURI(tokenId);
+    }
+
+    function next_itration() public {
+        _iterationCounter.increment();
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _tokenIdCounter.current();
     }
 
     /////////////////////////////////////////////THIS FUNCTION IS JUST FOR TEST PURPOSES (!!0) --- !!2
